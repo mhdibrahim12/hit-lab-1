01ee4e0427034f77f6f74e931080af03f9773898
mhdibrahim12 <84727184+mhdibrahim12@users.noreply.github.com>
2021-06-06 15:45:58 +0530

    main

---
+++ Demo.java
@@ -0,0 +1,46 @@
+package fire;
+import java.util.Scanner;
+public class demo {
+
+	public static void main(String[] args) {
+		int year;
+		Scanner scan = new Scanner (System.in);
+		System.out.println("Enter any Year");
+		year = scan.nextInt();
+		scan.close();
+		boolean isleap = false;
+		
+		if( year % 4 == 0)
+		{
+			  if( year % 100 == 0)
+			  {
+				   if( year  % 400 == 0 )
+				   {
+					   isleap = true;
+				   }else
+				   {
+					   isleap = false;
+				   }
+				   
+			   }else
+			       {
+				    isleap = true;  
+			       }
+		}else
+	      	{ 
+			  isleap = false;
+	    	}
+		
+	  if( isleap ==true)
+	  {
+		  System.out.println(year + "is a leap year");
+	  }else
+	      {
+               System.out.println( year + "is not aleap year");		  
+	      }
+	   
+	
+	
+	}
+
+}
